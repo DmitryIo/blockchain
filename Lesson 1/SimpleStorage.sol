e2fa97db545cd84d7cbb4a85e1205fa95465441a
DmitryIo <enakentiy.lorens@yandex.ru>
2022-04-26 19:36:30 +0300

    First lesson

---
+++ Lesson 1/SimpleStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    // this will be get initializet to 0
+    uint256 favouriteNumber;
+    bool favouriteBool;
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    // dynamic array
+    People[] public people;
+    
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    // view просто для просмотра состояния блокчейна
+    // pure для какой-то математики
+    function viewNumber() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    function doubleNumber(uint256 _favouriteNumber) public view returns(uint256){
+        return favouriteNumber + _favouriteNumber;
+    }
+    // storage означает хранить переменную после execution
+    // memory означает хранить переменную только на время обработки
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        //people.push(People({favouriteNumber : _favouriteNumber, name: _name}));
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+    
+}
